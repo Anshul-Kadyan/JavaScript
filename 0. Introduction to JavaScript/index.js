ceb72d742e7c0057ed6173b3cd51faf9b90d21a9
Anshul-Kadyan <anshul.kadyan.124201@gmail.com>
2023-03-25 17:59:35 -0700

    Adding Introduction Folder

---
+++ 0. Introduction to JavaScript/index.js
@@ -0,0 +1,10 @@
+console.log("I like Ice Cream")
+console.log("It is really good!")
+console.alert("I really love Ice Cream")
+
+// This is Single line Comment
+
+/*
+        This is a Multi-Line
+        Comment.
+*/
