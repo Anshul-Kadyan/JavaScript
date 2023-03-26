a77dfc616b86dae27f4a0f613842aad6cae92306
Anshul-Kadyan <anshul.kadyan.124201@gmail.com>
2023-03-25 21:16:20 -0700

    Method Chaining

---
+++ Method Chaining/index.js
@@ -0,0 +1,9 @@
+// method chaining = calling one method after another
+//                   in one contionous line of code
+
+let userName = 'anshul'
+
+let letter = userName.charAt(0).toLocaleUpperCase()
+// letter = letter.toUpperCase()
+
+console.log(letter)
