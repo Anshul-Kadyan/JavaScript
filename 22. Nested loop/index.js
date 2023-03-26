b5309784984d79e719776f6c2c825878e9081643
Anshul-Kadyan <anshul.kadyan.124201@gmail.com>
2023-03-26 00:58:40 -0700

    nested loops

---
+++ 22. Nested loop/index.js
@@ -0,0 +1,12 @@
+// nested loop = a loop insdie of another loop
+
+let rows = window.prompt("Enter # of rows");
+
+let columns = window.prompt("Enter # of columns");
+
+for (let i = 1; i <= rows; i+=1){
+    for (let j = 1; j <= columns; j+=1){
+        document.getElementById('myRectangle').innerHTML += '*'
+    }
+    document.getElementById('myRectangle').innerHTML += "<br>"
+} 
