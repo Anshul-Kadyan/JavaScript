f4b680d988a19ff8e368f4488f5b54b729018dd3
Anshul-Kadyan <anshul.kadyan.124201@gmail.com>
2023-03-30 11:13:28 -0700

    arrow fuction

---
+++ 044. Arrow Function/index.js
@@ -0,0 +1,28 @@
+// arrow function = compace alternative to traditional function
+//      =>
+
+/*
+
+const greeting = function(userName){
+    console.log(`Hello ${userName}`)
+}
+
+greeting("Anshul")
+
+*/
+
+/*
+
+const greeting = (userName) => console.log(`Hello ${userName}`)
+greeting("Anshul");
+
+*/
+
+const percent = (x, y) => x / y * 100;
+
+console.log(`${percent(45, 50)} %`)
+
+
+
+
+
