4db2db0718c51465e3609544d74b41362ed7d695
Anshul-Kadyan <anshul.kadyan.124201@gmail.com>
2023-04-02 06:04:50 -0700

    static keyword

---
+++ 052. Static Keyword/index.js
@@ -0,0 +1,30 @@
+// static = belongs to class, not the objects,
+//          properties: useful for caches, fixed-congiguration
+//          methods:    useful for utility functions
+
+
+class Car{
+
+   static numberOfCars = 0;
+
+   constructor(model){
+      this.model = model
+      Car.numberOfCars += 1; 
+   }
+
+   static startRace(){
+      console.log('3...2...1...GO!')
+   }
+}
+
+const car1 = new Car("Mustang");
+const car2 = new Car("Corvette");
+const car3 = new Car("BMW");
+
+console.log(Car.numberOfCars);
+Car.startRace()
+
+
+
+
+
