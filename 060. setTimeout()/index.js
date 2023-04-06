052f460c67ef097e977e731e0dfe915aefb894a7
Anshul-Kadyan <anshul.kadyan.124201@gmail.com>
2023-04-06 05:05:49 -0700

    setTimeout()

---
+++ 060. setTimeout()/index.js
@@ -0,0 +1,36 @@
+// setTimeout() = invokes a function after a number of miliseconds
+//                asynchronous function (doesn't pause execution)      
+
+let item = 'cryptocurrency'
+let price = 420.69
+
+
+let timer1 = setTimeout(firstMessage, 3000, item, price)
+let timer2 = setTimeout(secondMessage, 6000)
+let timer3 = setTimeout(thirdMessage, 9000)
+
+
+function firstMessage(item, price){
+    alert(`Buy this ${item} for Rs. ${price}!`)
+}
+
+function secondMessage(){
+    alert(`This is not a scam!`)
+}
+
+function thirdMessage(){
+    alert(`Do ITT!`)
+}
+
+document.getElementById('myButton').onclick = function(){
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    clearTimeout(timer3);
+    alert('Do it!')
+}
+
+
+
+
+
+
